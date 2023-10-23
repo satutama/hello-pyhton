08e2c17ee77e583b00ce59bc3ba7a9266d3bda86
Satria Utama <satria.u.s@gmail.com>
2023-10-23 17:37:36 +0200

    user input

---
+++ chapter2.py
@@ -0,0 +1,8 @@
+variableRule1= 'Must start with a letter or underscore _'
+variableRule2= 'Must consist of letters, numbers, and underscores'
+variableRule3= 'Case Sensitive'
+
+print(type(variableRule1))
+
+name = input('Who are you? ')
+print('Welcome', name)
