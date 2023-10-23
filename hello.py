7eecbe90483921c37e812ca4825c208e1546c866
Satria Utama <satria.u.s@gmail.com>
2023-10-23 16:09:52 +0200

    conditional steps

--- hello.py
+++ hello.py
@@ -7,4 +7,13 @@ print(msg)
 print(np.random.randint(1,9))
 
 print(msg1)
-print(np.random.randint(1,6))
+x = np.random.randint(1,6)
+print(x)
+
+if x < 10:
+    print('smaller')
+
+if x > 20:
+    print('biger')
+
+print('Finish')
