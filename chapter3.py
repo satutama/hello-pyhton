b466197ed8ff280f6c077e6192e0baf8e6daf657
Satria Utama <satria.u.s@gmail.com>
2023-10-24 18:50:34 +0200

    indentation

--- chapter3.py
+++ chapter3.py
@@ -13,3 +13,11 @@ if x <= 5:
     print('Less than or equal 5')
 if x != 6:
     print('Not equal 6')
+
+
+# Increase indent after an if statement or for statement (after :)
+# Maintain indent to indicate scope of the block
+# Reduce indent back to indicate the end of bloc
+# Blank lines are ignored - they do not affect indentation
+# Comments on  a line by themselves are ignored with regard to indentation
+
