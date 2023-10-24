f0e9c2f2de382a84e7121c89d7b1ab501560fc23
Satria Utama <satria.u.s@gmail.com>
2023-10-24 18:46:00 +0200

    conditional execution and comparison operators

---
+++ chapter3.py
@@ -0,0 +1,15 @@
+#Conditional execution & Comparison operators
+
+x = 5
+if x == 5:
+    print('Equals 5')
+if x > 4:
+    print('Greater than 4')
+if x >= 5:
+    print('Greater than or equal 5')
+if x < 6:
+    print('Less than 6')
+if x <= 5:
+    print('Less than or equal 5')
+if x != 6:
+    print('Not equal 6')
