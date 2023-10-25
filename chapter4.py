847ab8f0d8af964c52bd1eaed73f804da052cbae
Satria Utama <satria.u.s@gmail.com>
2023-10-25 11:35:53 +0200

    arguments and parameter

--- chapter4.py
+++ chapter4.py
@@ -13,3 +13,16 @@ big = max(hello)
 tiny = min(hello)
 print(big)
 print(tiny)
+
+#Arguments and parameter
+def greet(lang):
+    if lang == 'es':
+        print('Hola')
+    elif lang == 'fr':
+        print('Bonjour')
+    else:
+        print('Hello')
+
+greet('es')
+greet('fr')
+greet('test')
