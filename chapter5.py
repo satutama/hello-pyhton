304e82445fe525022f47c67ee1f32e268f9a21a6
Satria Utama <satria.u.s@gmail.com>
2023-10-26 11:35:35 +0200

    iteration with continue

--- chapter5.py
+++ chapter5.py
@@ -14,3 +14,13 @@ while True:
         break
     print(line)
 print('Done!')
+
+# finishing an iteration with continue
+while True:
+    line = input('> ')
+    if line[0] == '#':
+        continue
+    if line == 'done':
+        break
+    print(line)
+print('Done!')
