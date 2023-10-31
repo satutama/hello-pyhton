3b62694d6678bfbbfe60c3cba21233d08ed817c4
Satria Utama <satria.u.s@gmail.com>
2023-10-31 15:34:15 +0400

    summing in a loop

--- chapter5.py
+++ chapter5.py
@@ -56,3 +56,11 @@ for thing in [9,41,12,3,74,15]:
     zork = zork + 1
     print(zork, thing)
 print('After', zork)
+
+# Summing in a loop
+zork = 0
+print('Before', zork)
+for thing in [9,41,12,3,74,15]:
+    zork = zork + thing
+    print(zork, thing)
+print('After', zork)
