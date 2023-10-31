57b90ff4cb0ea471ea31ad2c6dbc8960ad08bef4
Satria Utama <satria.u.s@gmail.com>
2023-10-31 15:38:01 +0400

    find average in loop

--- chapter5.py
+++ chapter5.py
@@ -64,3 +64,13 @@ for thing in [9,41,12,3,74,15]:
     zork = zork + thing
     print(zork, thing)
 print('After', zork)
+
+# Find average in a loop
+count = 0
+sum = 0
+print('Before', count, sum)
+for thing in [9,41,12,3,74,15]:
+    count = count + 1
+    sum = sum + thing
+    print(count, sum, thing)
+print('After', count, sum, sum/count)
