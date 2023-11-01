fd96699112b70cad36d8f1d8a732a513a98aca0b
Satria Utama <satria.u.s@gmail.com>
2023-11-01 13:30:14 +0400

    Slicing strings

--- chapter6.py
+++ chapter6.py
@@ -22,4 +22,17 @@ countLetterA = 0
 for letter in fruit:
     if letter == 'a':
         countLetterA = countLetterA + 1
-print(countLetterA)
+# countLetterA is 3
+
+# Slicing strings
+s = 'Monty Python'
+print(s[0:4]) # Mont
+print(s[6:7]) # P
+print(s[6:20]) # Python
+print(s[:2]) # Mo
+print(s[8:]) # thon
+print(s[:]) # Monty Python
+# s[start from, up to but not including]
+# if the second number is beyonf the end of the string, it stops at the end
+# if we omit the first, it is assumed to be the begining of the string
+# if we omit the second, it is assumed to be the end of the string
