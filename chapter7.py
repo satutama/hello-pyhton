012372bc180187a151d7acda5b1fc23f37653ba5
Satria Utama <satria.u.s@gmail.com>
2023-11-01 14:43:26 +0400

    newline character

--- chapter7.py
+++ chapter7.py
@@ -3,3 +3,7 @@
 fhand = open('mbox.txt') # open(fileName, mode) mode by default is 'r' (read). Other node us 'w'(Write)
 print(fhand)
 
+# Newline character
+stuff = 'Hello\nWorld'
+print(stuff)
+#newline is 1 character not 2
