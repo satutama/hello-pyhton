3b66ed5ac4714a80672116c2c636379f9a2a021a
Satria Utama <satria.u.s@gmail.com>
2023-11-01 12:48:32 +0400

    while loop in string

--- chapter6.py
+++ chapter6.py
@@ -5,3 +5,9 @@ characterLength = len(fruit) # 6
 letter = fruit[1] # a
 # you will get python error if index beyond the end of a string
 
+# Looping through strings - while
+index = 0
+while index < characterLength:
+    letter = fruit[index]
+    print(letter)
+    index = index + 1
