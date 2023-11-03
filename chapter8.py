eac3b6a1100d3a48b646fce07746517cb07aa40c
Satria Utama <satria.u.s@gmail.com>
2023-11-03 22:08:18 +0400

    lists and definit loops

---
+++ chapter8.py
@@ -0,0 +1,11 @@
+# List
+# A list element can be any Python object
+# A list can be empty
+
+listInAnotherlist = [1,[11,5], 2]
+
+# Lists and definite loops - best pals
+friends = ['Satria', 'Kana', 'Gwynn']
+for friend in friends:
+    print('Happy new year', friend)
+print('done')
