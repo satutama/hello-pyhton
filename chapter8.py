84d82af43fdb35072c60ac65ca74f5df498ddefa
Satria Utama <satria.u.s@gmail.com>
2023-11-03 22:13:31 +0400

    lists are mutable

--- chapter8.py
+++ chapter8.py
@@ -9,3 +9,14 @@ friends = ['Satria', 'Kana', 'Gwynn']
 for friend in friends:
     print('Happy new year', friend)
 print('done')
+
+# strings are immutable - cannot change the content of a string - we must make a new string to make any change
+# lists are mutable - we can change an element of a list using the index operator
+
+# fruit = 'Banana'
+# fruit[0] = 'b' 
+# above will result type error
+
+lotto = [2,14,2,42,5,2]
+lotto[2] = 28 
+# lotto = [2,14,28,42,5,2]
