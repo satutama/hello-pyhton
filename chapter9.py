4b35e9fe0daee454a022cbf6a783bd488c594886
Satria Utama <satria.u.s@gmail.com>
2023-11-07 17:21:57 +0100

    what is a collection

---
+++ chapter9.py
@@ -0,0 +1,7 @@
+# Pyhton dictionaries
+
+# What is a Collection
+# we can put more than one value in it and carry them all around in one convenient package
+# we have a bunch of values in a single 'variable'
+# we do this by having more than one place 'in' the variable
+# we have ways of finding the different places in the variable 
