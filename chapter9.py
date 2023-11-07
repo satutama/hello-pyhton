938d525f72076bcbe6970a07497355040889aad0
Satria Utama <satria.u.s@gmail.com>
2023-11-07 17:27:11 +0100

    Dictionaries definition

--- chapter9.py
+++ chapter9.py
@@ -5,3 +5,13 @@
 # we have a bunch of values in a single 'variable'
 # we do this by having more than one place 'in' the variable
 # we have ways of finding the different places in the variable 
+
+# List is a linear collection of values that stay in order
+# Dictionary is a 'bag'of values, each with its own label
+
+# Dictionaries are python's most powerful data collection
+# Dictionaries allow us to do fast database-like opration in Python
+# Dictionaries have different names in different languages
+    # Associative Arrays - Perl/PHP
+    # Properties or Map or HashMap - Java
+    # Property Bag  C# / .Net
