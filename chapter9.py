f191ee41fbe0420f6f720d71a687f44959219372
Satria Utama <satria.u.s@gmail.com>
2023-11-07 17:30:58 +0100

    dictionary example

--- chapter9.py
+++ chapter9.py
@@ -1,13 +1,15 @@
 # Pyhton dictionaries
 
 # What is a Collection
-# we can put more than one value in it and carry them all around in one convenient package
-# we have a bunch of values in a single 'variable'
-# we do this by having more than one place 'in' the variable
-# we have ways of finding the different places in the variable 
+    # we can put more than one value in it and carry them all around in one convenient package
+    # we have a bunch of values in a single 'variable'
+    # we do this by having more than one place 'in' the variable
+    # we have ways of finding the different places in the variable 
 
 # List is a linear collection of values that stay in order
+# Lists index their entries based on the position in the list
 # Dictionary is a 'bag'of values, each with its own label
+# Dictionaries are like bag - no order. So we index the thintgs we put in dictionary with a 'lookup tag'
 
 # Dictionaries are python's most powerful data collection
 # Dictionaries allow us to do fast database-like opration in Python
@@ -15,3 +17,12 @@
     # Associative Arrays - Perl/PHP
     # Properties or Map or HashMap - Java
     # Property Bag  C# / .Net
+
+purse = dict()
+purse['money'] = 12
+purse['candy'] = 3
+purse['tissues'] = 75
+print(purse) # {'money': 12, 'candy': 3, 'tissues': 75}
+print(purse['candy']) # 3
+purse['candy'] = purse['candy'] + 2
+print(purse) # {'money': 12, 'candy': 5, 'tissues': 75}
