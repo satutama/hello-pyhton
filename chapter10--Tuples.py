42c92ca9bb375eccee9990362ee89f9d25c826ab
Satria Utama <satria.u.s@gmail.com>
2023-11-09 15:25:03 +0100

    tuples intro

---
+++ chapter10--Tuples.py
@@ -0,0 +1,10 @@
+# Tuples
+    # tuples are another kind of sequence that functions much like a list
+    # but.. tuples are 'immutable'
+    # Unlike a list, once you create a tuple, you cannot alter its content - similar to a string
+
+names = ('satty','gwyn','kana','mory')
+print(names[2])
+# names[2] = 'chantal'   ----> will return traceback: 'tubple' object does not support item Assignment
+
+# Things not to do with tuples, sort, append, reverse
