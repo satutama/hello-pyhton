c0d46217b6fb37c554e51a16efd9b1cf78c496d0
Satria Utama <satria.u.s@gmail.com>
2023-11-13 16:20:18 +0100

    Regex intro

---
+++ chapter11--RegEx.py
@@ -0,0 +1,6 @@
+# Regular Expressions
+    # In computing, a regular expression, also refered as "regex" or "regexp",
+    # provides a concise and flexible means for matching strings of text,
+    # such as particular characters, words, or patterns of characters.
+    # A regex is written in a formal language that can be interpreted by a regular expression processor.
+    # Basiccally, a really clever "Wild Card" expressions for matching and parsing strings
