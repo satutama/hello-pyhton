333a47cbc1d1ab7832791a7b55be780f573d0e03
Satria Utama <satria.u.s@gmail.com>
2023-11-15 10:32:37 +0100

    TCP intro

---
+++ chapter12--Networking.py
@@ -0,0 +1,24 @@
+'''
+# Networking with Python
+    # checkout transport control protocol for (TCP) details
+     
+# TCP connections / Sockets
+
+In computer networking, an internet socket or network socket is an endpoint
+of a bidirectional inter-process communication flow across an internet
+Protocol-based computer network, such as the Internet.
+
+Browser <-- Internet --> Web server
+
+    # TCP Port Numbers
+        - A port is an application-specific or process-specific software communications endpoint
+        - it allows multiple networked applications to coexist on the same server
+        - There is a list of well-known TCP port numbers
+
+        Common TCP Ports
+        . Telnet (23) - login
+        . SSH (22) - Secure login
+        . HTTP (80)
+        . HTTPS (443) - secure
+
+'''
