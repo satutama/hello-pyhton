dc740b46bb08d8319ceb45ca285e806ffa7cb0c9
Satria Utama <satria.u.s@gmail.com>
2023-11-16 15:16:13 +0100

    using urrlib in python

--- chapter12--Networking.py
+++ chapter12--Networking.py
@@ -106,5 +106,17 @@ mysock.close()
                                 <--                   <--    
                               decode()               recv()
 
+'''
 
-    '''
+# Using urllib in Python
+    # Since HTTP is so common, we have a library that does all the socket work for us and makes web pages look like a file
+
+import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+counts = dict()
+for line in fhand:
+    words = line.decode().split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+print(counts)
