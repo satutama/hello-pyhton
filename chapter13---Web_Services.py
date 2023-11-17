19b1f28fd0386269cbe902c440ad6a240cbc76f4
Satria Utama <satria.u.s@gmail.com>
2023-11-17 11:10:17 +0100

    Data on the web

---
+++ chapter13---Web_Services.py
@@ -0,0 +1,6 @@
+'''
+Data on the web
+    . There are 2 commonly used formats: XML and JSON to represent data going between aplication and across networks
+    . When we take the data from internal representation (python dictionary for example) and send it to the 'Wire', it's called serialization
+    . When we receive the data from the wire, we need to deserialize it (from XML or JSON), it's called deserialization
+'''
