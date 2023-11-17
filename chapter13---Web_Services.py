915b5a57e956151eb68ae6d4c61aeade9145cfe5
Satria Utama <satria.u.s@gmail.com>
2023-11-17 11:56:23 +0100

    xml in python examples

--- chapter13---Web_Services.py
+++ chapter13---Web_Services.py
@@ -41,3 +41,43 @@ XML - eXtensible Markup Language
             . xs:complexType
 
 '''
+
+# XML inside python example
+    # data should usually come from outside
+import xml.etree.ElementTree as ET
+data = '''<person>
+    <name>Sat</name>
+    <phone type="intl">
+        +31 234 56 778
+     </phone>
+     <email hide="yes"/>
+    </person>'''
+
+tree = ET.fromstring(data)
+print('Name:', tree.find('name').text)
+print('Attr:', tree.find('email').get('hide'))
+
+print("\n") # line break
+
+#Another example
+import xml.etree.ElementTree as ET
+input = '''<stuff>
+    <users>
+        <user x="2">
+            <id>001</id>
+            <name>Sat</name>
+        </user>
+        <user x="8">
+            <id>007</id>
+            <name>Bond</name>
+        </user>
+    </users>
+    </stuff>'''
+
+stuff = ET.fromstring(input)
+lst = stuff.findall('users/user')
+print('User count:', len(lst))
+for item in lst:
+    print('Name:', item.find('name').text)
+    print('Id:', item.find('id').text)
+    print('Attribute:', item.get("x"))
