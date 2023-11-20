04e24e9495c86d577b3647f7135b5b8f9f6c3845
Satria Utama <satria.u.s@gmail.com>
2023-11-20 15:00:20 +0100

    API example

--- chapter13--Web_Services-II.py
+++ chapter13--Web_Services-II.py
@@ -56,6 +56,14 @@ Service Oriented Approach
 Application Program Interface (API)
     The API itself is largely abstract in that it specifies an interface and controls the behavior of the objects specified in that interface.
     The software that provides the functionality described by an API is said to be an "implementation" of the API.
-    An API is typically defined in terms of the programming language used to build an application.
+    An API is typically defined in terms of the programming language used to build an application. (contract for interaction)
+
+    API Security and Rate limiting
+        . The compute resources to run these APIs are not "free"
+        . The data provided by these APIs is usually valuable
+        . The data providers might limit the number of requests per day, demand an API "key", or even charge for usage
+        . They might change the rules as things progress
+
+*Check API example folder
 
 '''
