1c4f89a8a571cb5ac785a7af32dbed4878d85f89
Satria Utama <satria.u.s@gmail.com>
2023-11-20 14:47:46 +0100

    API

--- chapter13--Web_Services-II.py
+++ chapter13--Web_Services-II.py
@@ -53,4 +53,9 @@ Service Oriented Approach
     . They use services from other applications (credit card charge, Hotel reservation system)
     . Services publish the "rules" applications must follow to make use of the service (API)
 
+Application Program Interface (API)
+    The API itself is largely abstract in that it specifies an interface and controls the behavior of the objects specified in that interface.
+    The software that provides the functionality described by an API is said to be an "implementation" of the API.
+    An API is typically defined in terms of the programming language used to build an application.
+
 '''
