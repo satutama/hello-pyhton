9d733f490e00aa41e71cc163d0b5a4e100b4d89b
Satria Utama <satria.u.s@gmail.com>
2023-11-21 15:53:06 +0100

    Object terminology

---
+++ chapter14--Objects.py
@@ -0,0 +1,20 @@
+'''
+Python Objects
+
+    Object Oriented
+        . A program is made up of many cooperating objects
+        . Instead of being the "whole program"- each object is a little "island" withing the program and cooperatively working with oter objects
+        . A program is made up of one or more objects working together - objects make use of each other's capability
+
+    Object
+        . An object is a bit of self-contained Code and Data
+        . A key aspect of the Object approach is to break the problem into smaller understandable parts (divide and conquer)
+        . Objects have boundaries that allow is to ignore un-needed detail
+        . We have been using objects all along: String Objects, Integer Objects, Dictionary Objects, List Objects...
+
+    Definitions
+        . Class - a template - Dog
+        . Method or Message - A defined capability of a class - bark()
+        . Field or attribute - A bit of data in a class - length
+        . Object or Instance - A particular instance of a class - Lassie
+'''
