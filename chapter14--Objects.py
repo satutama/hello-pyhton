a1c9012245907a784f775995dd816e8dcf92d1e3
Satria Utama <satria.u.s@gmail.com>
2023-11-22 15:00:03 +0100

    inheritance

--- chapter14--Objects.py
+++ chapter14--Objects.py
@@ -98,3 +98,13 @@ an.party()
 an.party()
 an = 42
 print('an contains', an)
+
+
+'''
+Inheritance
+    . When we make a new class - we can reuse an existing class and INHERIT all the capabilities of an existing class and the add our own little bit to make our new class
+    . Another form of store and reuse
+    . Write once - reuse many times
+    . The new class (child) has all the capabilities of the old class (parent) - and the some more
+'''
+
