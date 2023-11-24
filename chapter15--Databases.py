d6107a8ee5b5f48741dfe3d039d2d2483e2e63c3
Satria Utama <satria.u.s@gmail.com>
2023-11-24 12:37:27 +0100

    Database model

--- chapter15--Databases.py
+++ chapter15--Databases.py
@@ -18,5 +18,22 @@ SQL - Structured Query Language
     . Insert data
     . Delete data
 
+Web Applications w/ Databases
+    . Application Developer - Builds the logic for the application, the look and feel of the application - monitors the application for problems
+    . Database Administrator - Monitors and adjusts the database as the program runs in production
+    . Often both people participate in the building of the "Data model"
+
+Database Model 
+    or database schema is the structure or format of a database, described in a formal language supported by the database managbement system.
+    In other words, a 'database model'is the application of a data model when used in conjunction with a database management system.
+
+Common Database Systems
+    Three major Database Management Systems:
+    . Oracle - Large, commercial, enterprise-scale, very very tweakable
+    . MySql - simpler but very fast an scalable - commercial open source
+    . SqlServer - very nice - from microsoft
+
+    Smaller projects, free and open source
+    . HSQL, SQLite, Postgres,...
 
 '''
