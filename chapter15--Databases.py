5082d8d5cb8ac067d5aa2301b45a4c3599abdbcb
Satria Utama <satria.u.s@gmail.com>
2023-11-24 12:25:16 +0100

    Relational databases 101

---
+++ chapter15--Databases.py
@@ -0,0 +1,14 @@
+'''
+Relational Databases
+    Relational databases model data by storing rows and columns in tables.
+    The power of the relational databases lies in its ability to efficiently retrieve data from those tables
+    and in particular where there are multiple tables and the relationships between those tables involed in the query.
+
+    Terminology
+        Database - contains many tables
+        Relation (or table) - contains tuples that have the same attributes
+        Tuple (or row) - a set of fields that generally represents an "object" like a person or a music trac
+        Attribute (also column or field) - on of possibly many elements of data corresponding to the object represented by the row
+
+    
+'''
