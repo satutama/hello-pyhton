37a9be65e0448b588826a2c647a1f9f35bf52d1d
Satria Utama <satria.u.s@gmail.com>
2023-11-28 14:38:41 +0100

    student-courses table

--- chapter15--Databases.py
+++ chapter15--Databases.py
@@ -117,5 +117,13 @@ Many-To-Many Relationships
     . Id                    . user_id               . id
     . title                 . course_id             . name
 
+    Below is the SQL example
 
+    SELECT User.name, Member.role, Course.title
+    FROM User JOIN Member JOIN Course
+    ON Member.user_id = User.id AND Member.course_id = Course.id
+    ORDER BY Course.title, Member.role DESC, User.name 
+
+        
+    
 '''
