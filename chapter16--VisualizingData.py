40027a4cb93bf93863cc2614626330bc5b8664cd
Satria Utama <satria.u.s@gmail.com>
2023-11-29 14:34:56 +0100

    visualizing 101

---
+++ chapter16--VisualizingData.py
@@ -0,0 +1,20 @@
+'''
+Visualizing Data with Python
+    
+    Multi-Step Data Analysis
+    Data source -- Gather --> Databases -- Clean/Process --> Data -- Visualiza/Analyze
+
+    Many Data Mining Technologies
+    . https://hadoop.apache.org/
+    . https://spark.apache.org/
+    . https://aws.amazon.com/redshift/
+    . https://community.pentaho.com/
+    . ...
+
+
+    GeoData - check opengeo folder
+    . Makes a google map from user entered data
+    . uses the google geodata API
+    . caches data in a database to avoid rate limiting and allow restarting
+    . visualized in a browser using the google maps API
+'''
